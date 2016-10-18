ec8f6da55e88c624e12e375857f2c47cf47e48a3
Erik Miller <emiller46@radford.edu>
2016-10-18 16:57:35 -0400

    updated views before presentation

--- resources/views/userViews/index.blade.php
+++ resources/views/userViews/index.blade.php
@@ -5,10 +5,40 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Testing Dashboard page</div>
+                <div class="panel-heading">Welcome to your dashboard!</div>
 
                 <div class="panel-body">
-                    Dashboard Page.
+						<table style="float: left">
+							<tr>
+							  <th>Groups: </th>
+							</tr>
+							<tr>
+							  <td>Wildcats 2.0</td>
+							</tr>
+							<tr>
+							  <td>Planters Pals</td>
+							</tr>
+							<tr>
+							  <td>Planters Pals</td>
+							</tr>
+						</table>
+						<table>
+							<tr>
+								<th>Tasks: </th>
+							</tr>
+							<tr>
+								<td>Finish the project</td>
+							</tr>
+							<tr>
+								<td>Start the project</td>
+							</tr>
+							<tr>
+								<td>Do the stuff</td>
+							</tr>
+							<tr>
+								<td>Do the stuff</td>
+							</tr>
+						</table>
                 </div>
             </div>
         </div>
--- resources/views/userViews/user_edit.blade.php
+++ resources/views/userViews/user_edit.blade.php
@@ -1,14 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+#form {
+    background-color: #FFF;
+    height: 600px;
+    width: 600px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    padding: 0px;
+    text-align:center;
+}
+label {
+    font-family: Georgia, "Times New Roman", Times, serif;
+    font-size: 18px;
+    color: #333;
+    height: 20px;
+    width: 200px;
+    margin-top: 10px;
+    margin-left: 10px;
+    text-align: right;
+    clear: both;
+    float:left;
+    margin-right:15px;
+}
+input {
+    height: 20px;
+    width: 300px;
+    border: 1px solid #000;
+    margin-top: 10px;
+    float: left;
+}
+input[type=button] {
+    float:none;
+}
+    </style>
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Testing the edit page</div>
-
+                <div class="panel-heading">Edit user information</div>
                 <div class="panel-body">
-                    Edit page.
+					<div id="form">
+						<form action="" method="post" name="registration" class="register">
+							<fieldset>
+								<label for="Student">Name:</label>
+								<input name="Student" />
+								<label for="Phone_Number">Phone number:</label>
+								<input name="Phone_Number" />
+								<label for="Email">Email:</label>
+								<input name="Email" />
+								<label for="Username">Username:</label>
+								<input name="Username" />
+								<label for="Password">Password:</label>
+								<input name="Password" type="password" />
+							</fieldset>
+								{{--<input name="delbutton" type="button" class="button" value="Delete Account" />--}}
+                            <button type="submit" name="regbutton" type="button" class="btn btn-primary">Update Information</button>
+						</form>
+                        <br/>
+                        <a href="{{ url('/user_delete') }}" class="btn btn-danger">Delete Account</a>
+					</div>
                 </div>
             </div>
         </div>
