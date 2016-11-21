b1c4bdcff11830e4a70980d64798c1c484b9ef90
Unknown <ccaldwell23@radford.edu>
2016-11-21 14:02:17 -0500

    Error reporting for phone, input phone to database from register page, created seperate tables for group and tasks on homepage

--- resources/views/userViews/index.blade.php
+++ resources/views/userViews/index.blade.php
@@ -5,40 +5,60 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to your dashboard!</div>
-
+					
                 <div class="panel-body">
-						<table style="float: left">
-							<tr>
-							  <th>Groups: </th>
-							</tr>
-							<tr>
-							  <td>Wildcats 2.0</td>
-							</tr>
-							<tr>
-							  <td>Planters Pals</td>
-							</tr>
-							<tr>
-							  <td>Planters Pals</td>
-							</tr>
-						</table>
-						<table>
-							<tr>
-								<th>Tasks: </th>
-							</tr>
-							<tr>
-								<td>Finish the project</td>
-							</tr>
-							<tr>
-								<td>Start the project</td>
-							</tr>
-							<tr>
-								<td>Do the stuff</td>
-							</tr>
-							<tr>
-								<td>Do the stuff</td>
-							</tr>
-						</table>
+                    <table style="width:50%; float:left; top:0; bottom:0; left:100; right:0; border:0px">
+						<tr style="height:10%;">
+							<td>Groups</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+						<tr>
+							<td>Group Names from database here</td>
+						</tr>
+					</table>
+					<table style="width:50%; float:right; top:0; bottom:0; left:0; right:0; border:0px">
+						<tr>
+							<td>Tasks</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+						<tr>
+							<td>Tasks and descriptions from database here</td>
+						</tr>
+					</table>
                 </div>
             </div>
         </div>
